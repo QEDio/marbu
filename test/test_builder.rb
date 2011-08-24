9f8e4992f68ae9c97c9b8fcbd32c58901ffe186f
Johannes A Kaefer <jak4@gmx.net>
2011-08-24 19:51:24 +0200

    remove builder tests, refactored into mongodb builder

--- test/test_builder.rb
+++ test/test_builder.rb
@@ -1,37 +1,6 @@
 require File.dirname(__FILE__) + '/test_helper.rb'
 
 class TestBuilder < Test::Unit::TestCase
-  context "intializing a map_reduce object" do
-    setup do
-      @mrm        = Marbu::MapReduceModel.new(MR_WWB_LOC_DIM0)
-      @builder    = Marbu::Builder.new( @mrm)
-    end
-
-    should "create a map query" do
-      assert @builder.map.size > 0, "Didn't return a correctly build map object'"
-    end
-
-    should 'create the correct map query' do
-      assert_equal MAP_FUNCTION_ONE_LINE, Marbu::Formatter::OneLine.perform(@builder.map)
-    end
-
-    should "create a reduce query" do
-      assert @builder.reduce.size > 0, "Didn't return a correctly build map object'"
-    end
-
-    should 'create the correct reduce query' do
-      assert_equal REDUCE_FUNCTION_ONE_LINE, Marbu::Formatter::OneLine.perform(@builder.reduce)
-    end
-
-    should "create a finalize query" do
-      assert @builder.finalize.size > 0, "Didn't return a correctly build map object'"
-    end
-
-    should 'create the correct finalize query' do
-      assert_equal FINALIZE_FUNCTION_ONE_LINE, Marbu::Formatter::OneLine.perform(@builder.finalize)
-    end
-  end
-
   context "when requesting the map code" do
     context "and no map emit keys are defined it" do
       setup do
