a17902356a63818195c93ce6226fe7f43623167e
Johannes A Kaefer <jak4@gmx.net>
2011-08-24 19:50:51 +0200

    tst mongodb builder directly

---
+++ test/builders/test_mongodb_helper.rb
@@ -0,0 +1,5 @@
+unless Kernel.const_defined?("MONGODB_MAP_FUNCTION_ONE_LINE")
+  MONGODB_MAP_FUNCTION_ONE_LINE       = "function(){value=this.value;count=1;emit(value.DIM_LOC_0,{DIM_LOC_0:value.DIM_LOC_0,count:count});}"
+  MONGODB_REDUCE_FUNCTION_ONE_LINE    = "function(key,values){value=values[0];varcount=0;values.forEach(function(v){count+=v.count;});return{DIM_LOC_0:value.DIM_LOC_0,count:count};}"
+  MONGODB_FINALIZE_FUNCTION_ONE_LINE  = "function(key,value){return{DIM_LOC_0:value.DIM_LOC_0,count:value.count};}"
+end
