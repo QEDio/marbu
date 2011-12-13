79cac3b3a1f67ab0dfdde39e66c463d0ca5b8e18
Johannes Kaefer <jak4@lieserl.(none)>
2011-12-13 08:00:26 +0100

    option in query-class to make query datetime parameters mandatory in query

--- lib/marbu/models/mrf/query.rb
+++ lib/marbu/models/mrf/query.rb
@@ -2,16 +2,19 @@ module Marbu
   module Models
     class Query
       attr_accessor :condition, :force_query, :time_params
+      attr_accessor :datetime_mandatory
 
       def initialize( ext_params = {} )
-        params        = default_params.merge( ext_params.keep_if{|k,v|v} )
-        @condition    = params[:condition]
-        @force_query  = params[:force_query]
-        @time_params  = params[:time_params]
+        params                  = default_params.merge( ext_params.keep_if{|k,v|v} )
+        @condition              = params[:condition]
+        @force_query            = params[:force_query]
+        @time_params            = params[:time_params]
+        @datetime_mandatory     = params[:datetime_mandatory]
       end
 
       def default_params
         {
+          :datetime_mandatory       => false
         }
       end
 
@@ -25,9 +28,10 @@ module Marbu
 
       def serializable_hash
         {
-          :condition        => condition,
-          :force_query      => force_query,
-          :time_params      => time_params
+          :condition              => condition,
+          :force_query            => force_query,
+          :time_params            => time_params,
+          :datetime_mandatory     => datetime_mandatory
         }.delete_if{|k,v|v.blank?}
       end
     end
