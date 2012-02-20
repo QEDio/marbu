08fd1fd8e5280db57190c06d30564625d647ab81
Johannes Kaefer <jak4@jak4.com>
2012-02-20 17:02:39 +0100

    cleanup

--- bin/marbu-web-mongo-config.rb
+++ bin/marbu-web-mongo-config.rb
@@ -1,12 +1,10 @@
 require 'mongo'
 
-Marbu.database        = 'kp'
-Marbu.collection      = 'adwords_early_warning_staging'
 Marbu.uri             = '127.0.0.1'
 Marbu.port            = '27017'
 
 Marbu.storage         = {:database => 'kp', :collection => 'marbu', :uri => '127.0.0.1', :port => '27017'}
 
 Mongoid.configure do |config|
-  config.master = Mongo::Connection.new.db("marbu")
+  config.master = Mongo::Connection.new.db('marbu')
 end
